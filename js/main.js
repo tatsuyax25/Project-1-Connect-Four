91b0da0622dd5db342740edb1bebdd384e5d7e3a
Miguel Urena <44272798+tatsuyax25@users.noreply.github.com>
2021-09-20 21:04:41 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -129,6 +129,8 @@ function checkRight(colIdx, rowIdx) {
 	} 
 
 }
+// If a === b === c === d they are all the same color
+// if they are all the same color then declare a winner
 
 function checkUp(colIdx, rowIdx) {
 
